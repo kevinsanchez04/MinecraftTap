0c8ad57713933e2c4d2a4111dc977128e5481545
Iulian <93289638+IulianSebastian@users.noreply.github.com>
2024-12-07 19:50:23 +0100

    Gemini ChatBot

---
+++ AdventuresInMinecraft-PC-master/MyAdventures/Gemini.py
@@ -0,0 +1,28 @@
+import google.generativeai as genai
+from botFramework import BotFramework, BotManagerSingleton
+
+
+class Gemini(BotFramework):
+    
+    genai.configure(api_key="AIzaSyDQGxOj5gcORaUfp3sulsiDrEhAmcBfVhI")
+    model = genai.GenerativeModel("gemini-1.5-flash")    
+    clau = "gemini"
+    missatgeAnt = None
+
+    def doSomething(self):
+        chat = BotManagerSingleton().getChat()
+        if self.clau in chat and self.missatgeAnt != chat :
+            response = self.model.generate_content(chat)
+            self.mc.postToChat(response.text)
+            print(response.text)
+        self.missatgeAnt = chat
+
+    def getEvent(self):
+        return ":Gemini" #Cadena de caracters que actuara com una comanda d'activació del bot
+    
+    def __str__(self) -> str:
+        return "<Gemini> "
+    
+gemini = Gemini()
+
+BotManagerSingleton().managerThread.join()
